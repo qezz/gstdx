a278c5791d8d9a0d788979b35234619920cbaaec
Sergey Mishin <sergei.a.mishin@gmail.com>
2025-08-17 16:40:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# gstdx
+
+[![Package Version](https://img.shields.io/hexpm/v/gstdx)](https://hex.pm/packages/gstdx)
+[![Hex Docs](https://img.shields.io/badge/hex-docs-ffaff3)](https://hexdocs.pm/gstdx/)
+
+```sh
+gleam add gstdx@1
+```
+```gleam
+import gstdx
+
+pub fn main() -> Nil {
+  // TODO: An example of the project in use
+}
+```
+
+Further documentation can be found at <https://hexdocs.pm/gstdx>.
+
+## Development
+
+```sh
+gleam run   # Run the project
+gleam test  # Run the tests
+```
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Extended stdlib for Gleam";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            erlang
+            gleam
+            rebar3
+            erlang-ls
+          ];
+        };
+      }
+    );
+}
